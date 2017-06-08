27bc2bafd79fbefeabb073c8f4230b0faf47bc8d
Martin Strandgren <martin@emerbee.com>
2017-06-08 22:10:05 +0200

    Modified to suit me for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task("default", function () {
-  gulp.src('sass/style.sass')
-    .pipe(sass({outputStyle: 'compressed'}))
+  gulp.src('less/style.less')
+    .pipe(less({outputStyle: 'compressed'}))
     .pipe(autoprefixer())
     .pipe(gulp.dest('static/css/'));
 });
 
 gulp.task("watch", function () {
-  gulp.watch('sass/style.sass', function() {
-    gulp.src('sass/style.sass')
-      .pipe(sass({outputStyle: 'compressed'}))
+  gulp.watch('less/style.less', function() {
+    gulp.src('less/style.less')
+      .pipe(less({outputStyle: 'compressed'}))
       .pipe(autoprefixer())
       .pipe(gulp.dest('static/css/'));
   });
